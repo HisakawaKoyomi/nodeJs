9c2b951e2b56655c57093d1270fa16338a0053d4
HisakawaKoyomi <2094974654@qq.com>
2019-03-21 17:35:28 +0800

    user part #Lab-2019.3.21

--- demo.js
+++ demo.js
@@ -1,17 +1,25 @@
-function sleep(second) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            reject('want to sleep~');
-        }, second);
-    })
+let str = " 41"
+let num = '';
+let flag = 0;
+let map = ['0','1','2','3','4','5','6','7','8','9'];
+let i = 0;
+
+while (flag !== 2 && i < str.length){
+    console.log(0);
+    if (str[i] in map){
+        num += str[i];
+        flag = 1;
+        console.log(1);
+    }
+    if (!(str[i] in map) && str[i] != ' '){
+        flag = 2;
+        console.log(2);
+    }
+    if (str[i] == ' ' && flag == 1){
+        flag = 2;
+        console.log(3);
+    }
+    i++;
 }
 
-async function errorDemoSuper() {
-    try {
-        let result = await sleep(1000);
-        console.log(1);
-    } catch (err) {
-        console.log(err);
-    }
-}
-errorDemoSuper();
+console.log(+num);
--- wp/index.js
+++ wp/index.js
@@ -1,15 +1,63 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const multer = require("multer");
+const Multer = require("multer");
 const mysql = require("mysql");
 
 const server = express();
 server.listen(9111);
+server.use(Multer({dest: './www/allFiles'}).any()); //放在路由注册之后会导致存储路径改变！！
 
 //用户登录注册路由
 const loginRouter = express.Router();
 server.use('/login',loginRouter);
+
+//上传文件接口
+loginRouter.use('/getFiles', (req,res) => {
+
+    res.setHeader('Access-Control-Allow-Origin','*');
+
+    console.log(req.files);
+    var oldName = req.files[0].path;
+    var newName = req.files[0].path + path.parse(req.files[0].originalname).ext; //加了后缀的文件名
+    var hashName = req.files[0].filename + path.parse(req.files[0].originalname).ext; //后台处理获取哈希名
+    var thisTime = new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString(); //后台处理获取日期
+
+    fs.rename(oldName,newName,(err) => {
+        if (err){
+            console.log(err);
+            res.send({'ok': 0,'msg': '文件上传转码失败'});
+        }else {
+            var Pool = mysql.createPool({
+                'host': 'localhost',
+                'user': 'root',
+                'password': '1996',
+                'database': 'wp'
+            });
+            Pool.getConnection((err,connection) => {
+                if (err){
+                    console.log(err);
+                    res.send({'ok': 0,'msg': '数据库链接失败'});
+                    connection.release();
+                }else {
+                    connection.query('INSERT INTO `' + req.body.fsUser + '` (`lastName`,`hashName`,`size`,`type`,`download`,`lastTime`) VALUES ("' + req.files[0].originalname + '","' + hashName + '","' + req.files[0].size + '","' + path.parse(req.files[0].originalname).ext + '","0","' + thisTime + '");',(err,data) => {
+                        if (err){
+                            console.log(err);
+                            res.send({'ok': 0,'msg': '数据库写入失败'});
+                            connection.release();
+                        }else {
+                            res.send({'ok': 1,'msg': '上传成功','hash': hashName,'timer': thisTime});
+                        }
+                        connection.release();
+                    })
+                }
+            })
+        }
+    })
+
+
+
+});
 
 //用户注册
 loginRouter.use('/res',(req,res) => {
