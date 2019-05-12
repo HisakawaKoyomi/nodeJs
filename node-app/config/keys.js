d0f227883b5e1f647fe883819541c5cbd8a445a7
HisakawaKoyomi <14yli3@stu.edu.cn>
2019-05-12 20:19:16 +0800

    后端接口搭建 #dorm -2019.4.30-2019.5.12

---
+++ node-app/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:  "mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]",
+    // Atlas 提供的 mongoURI
+    secretOrKey: "secret"
+};
